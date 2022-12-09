c49af2f622d03b265c4792e03c5316e53874ecf5
Jason M <hack-r@users.noreply.github.com>
2022-12-08 22:08:17 -0500

    Update pyserpZotero.py
    Make it a little closer to being a library

--- pyserpZotero.py
+++ pyserpZotero.py
@@ -29,123 +29,147 @@ from rispy import TAG_KEY_MAPPING
 from serpapi import GoogleSearch
 from urllib.parse import urlencode
 
-# User Parameters
-API_KEY = ""
-ZOT_ID = ""
-ZOT_KEY = ""
-TERM = ""
-MIN_YEAR = ""
+class serpZot:
+    """
+    :param API_KEY: serpAPI API key
+    :type API_KEY: str
 
-# Search Parameters 
-params = {
-  "api_key": API_KEY,
-  "device": "desktop",
-  "engine": "google_scholar",
-  "q": TERM,
-  "hl": "en",
-  "num": "20",
-  "as_ylo": MIN_YEAR
-}
+    :param ZOT_ID: Zotero library Id
+    :type ZOT_ID: str
 
-# Search
-search = GoogleSearch(params)
+    :param ZOT_KEY: Zotero API key
+    :type ZOT_KEY: str
+    """
+ 
+    def __init__(self, API_KEY  = "", ZOT_ID = "", ZOT_KEY = ""):
+        self.API_KEY = API_KEY
+        self.ZOT_ID = ZOT_ID
+        self.ZOT_KEY = ZOT_KEY
 
-# Scrape Results, Extract Result Id's
-json_data = search.get_raw_json()
-data = json.loads(json_data)
-df = pd.json_normalize(data['organic_results'])
-ris = df['result_id']
+    # User Parameters
+    def search2Zotero(TERM = "", MIN_YEAR = ""):
+        """
+        Search for journal articles and add them to your Zotero library.
 
-# Get the Citation!
-params = {
-  "api_key": API_KEY,
-  "device": "desktop",
-  "engine": "google_scholar_cite",
-  "q": ris[0]
-}
+        :param TERM: search term for Google Scholar
+        :type TERM: str
 
-search = GoogleSearch(params)
-citation = search.get_dict()
+        :param MIN_YEAR: oldest year to search on
+        :type MIN_YEAR: str
+        """    
 
-# Get APA Format Citation and Parse
-citation['citations'][1]['snippet']
+        # Search Parameters 
+        params = {
+          "api_key": API_KEY,
+          "device": "desktop",
+          "engine": "google_scholar",
+          "q": TERM,
+          "hl": "en",
+          "num": "20",
+          "as_ylo": MIN_YEAR
+        }
 
-# Cross-reference the Citation with Crossref to Get Bibtext
-base = 'https://api.crossref.org/works?query.'
-api_url = {'bibliographic':citation['citations'][1]['snippet']}
-url = urlencode(api_url)
-url = base+url
-response = requests.get(url)
+        # Search
+        search = GoogleSearch(params)
 
-# Parse Bibtext from Crossref
-jsonResponse = response.json()
-jsonResponse = jsonResponse['message']
-jsonResponse = jsonResponse['items']
-jsonResponse = jsonResponse[0]
-jsonResponse['DOI']
-curl_str = 'curl -LH "Accept: application/x-bibtex" http://dx.doi.org/' + jsonResponse['DOI']
+        # Scrape Results, Extract Result Id's
+        json_data = search.get_raw_json()
+        data = json.loads(json_data)
+        df = pd.json_normalize(data['organic_results'])
+        ris = df['result_id']
 
-# Write bibtext file
-text_file = open("auto_cite.bib", "w")
-n = text_file.write(result)
-text_file.close()
+        # Get the Citation!
+        params = {
+          "api_key": API_KEY,
+          "device": "desktop",
+          "engine": "google_scholar_cite",
+          "q": ris[0]
+        }
 
-# Parse bibtext
-with open('auto_cite.bib') as bibtex_file:
-    parser = BibTexParser()
-    parser.customization = bibtexparser.customization.author    
-    bib_database = bibtexparser.load(bibtex_file, parser=parser)
-bib_dict = bib_database.entries[0]
-print(bib_dict)
+        search = GoogleSearch(params)
+        citation = search.get_dict()
 
-# Connect to Zotero
-zot = zotero.Zotero(ZOT_ID, 'user', ZOT_KEY)
-template = zot.item_template('journalArticle') # Set Template
+        # Get APA Format Citation and Parse
+        citation['citations'][1]['snippet']
 
-# Populate Zotero Template with Data
-try:
-    template['publicationTitle'] = bib_dict['journal']
-except:
-    pass
-template['title'] = bib_dict['title']
-template['DOI'] = str(jsonResponse['DOI'])
-try:
-    template['accessDate'] = str(date.today())
-except:
-    pass
-try:
-    template['extra'] = str(bib_database.comments)
-except:
-    pass
-try:
-    template['url'] = bib_dict['url']
-except:
-    pass
-try:
-    template['volume'] = bib_dict['volume']
-except:
-    pass
-try:
-    template['issue'] = bib_dict['number']
-except:
-    pass
-try:
-    template['abstractNote'] = df['snippet'][0]
-except:
-    pass
+        # Cross-reference the Citation with Crossref to Get Bibtext
+        base = 'https://api.crossref.org/works?query.'
+        api_url = {'bibliographic':citation['citations'][1]['snippet']}
+        url = urlencode(api_url)
+        url = base+url
+        response = requests.get(url)
 
-# Fix Date
-mydate = bib_dict['month']+' '+bib_dict['year']
-template['date'] = str(datetime.datetime.strptime(mydate, '%b %Y').date())
+        # Parse Bibtext from Crossref
+        jsonResponse = response.json()
+        jsonResponse = jsonResponse['message']
+        jsonResponse = jsonResponse['items']
+        jsonResponse = jsonResponse[0]
+        jsonResponse['DOI']
+        curl_str = 'curl -LH "Accept: application/x-bibtex" http://dx.doi.org/' + jsonResponse['DOI']
 
-# Parse Names into Template/Data
-num_authors = len(bib_dict['author'])
-template['creators'] = []
+        # Write bibtext file
+        text_file = open("auto_cite.bib", "w")
+        n = text_file.write(result)
+        text_file.close()
 
-for a in bib_dict['author']:
-    split = bibtexparser.customization.splitname(a, strict_mode=False)
-    template['creators'].append({'creatorType': 'author', 'firstName': split['first'][0], 'lastName': split['last'][0]})
+        # Parse bibtext
+        with open('auto_cite.bib') as bibtex_file:
+            parser = BibTexParser()
+            parser.customization = bibtexparser.customization.author    
+            bib_database = bibtexparser.load(bibtex_file, parser=parser)
+        bib_dict = bib_database.entries[0]
+        print(bib_dict)
 
-print(template)
+        # Connect to Zotero
+        zot = zotero.Zotero(ZOT_ID, 'user', ZOT_KEY)
+        template = zot.item_template('journalArticle') # Set Template
 
-zot.create_items([template])
+        # Populate Zotero Template with Data
+        try:
+            template['publicationTitle'] = bib_dict['journal']
+        except:
+            pass
+        template['title'] = bib_dict['title']
+        template['DOI'] = str(jsonResponse['DOI'])
+        try:
+            template['accessDate'] = str(date.today())
+        except:
+            pass
+        try:
+            template['extra'] = str(bib_database.comments)
+        except:
+            pass
+        try:
+            template['url'] = bib_dict['url']
+        except:
+            pass
+        try:
+            template['volume'] = bib_dict['volume']
+        except:
+            pass
+        try:
+            template['issue'] = bib_dict['number']
+        except:
+            pass
+        try:
+            template['abstractNote'] = df['snippet'][0]
+        except:
+            pass
+
+        # Fix Date
+        mydate = bib_dict['month']+' '+bib_dict['year']
+        template['date'] = str(datetime.datetime.strptime(mydate, '%b %Y').date())
+
+        # Parse Names into Template/Data
+        num_authors = len(bib_dict['author'])
+        template['creators'] = []
+
+        for a in bib_dict['author']:
+            split = bibtexparser.customization.splitname(a, strict_mode=False)
+            template['creators'].append({'creatorType': 'author', 'firstName': split['first'][0], 'lastName': split['last'][0]})
+
+        print(template)
+
+        zot.create_items([template])
+
+        return 0
