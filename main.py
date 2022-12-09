48962176c1dd31086fc19a918da51bf0142e3089
Jason M <hack-r@users.noreply.github.com>
2022-12-09 18:10:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pyserpCite
 
 
 # Instantiate a serpZot object for API management
-citeObj = pyserpCite.serpZot(API_KEY  = "2d7e471867259b72fb019add2ef26d4d477a87f913ef55d90b2c649f4b8af9de", 
-                           ZOT_ID   = "7032524", 
-                           ZOT_KEY  = "AHUiTfE6jN7hKweV5798MSrv")
+citeObj = pyserpCite.serpZot(API_KEY  = "", 
+                           ZOT_ID   = "", 
+                           ZOT_KEY  = "")
 
 # Call the search method
 print(citeObj.searchScholar(TERM = "memory oxygen treatment", MIN_YEAR = "2020"))
