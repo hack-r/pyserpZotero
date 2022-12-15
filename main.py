800b9406320fc0bde593df1923b233a4f84d747a
Jason M <hack-r@users.noreply.github.com>
2022-12-14 21:30:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 ########
 # Build a list of search terms:
-TERMS = ['prl-8-53','time series forecasting']
-MIN_YEAR = "2019"
-    
+TERMS = ['Gini Norm', 'Mean Absolute Percentage Error MAPE','Tweedie deviance','2SLS','3SLS','zero shot learning','one shot learning','hugging face', 'openai','gamma deviance', 'fraction of explained variance','Poisson deviance','zero-inflated time series','fraud imbalanced','imbalanced class']
+
+TERMS=['']
+
+MIN_YEAR = "2019" # Oldest year to search
+SAVE_BIB = False  # Save a Bibtex file (.bib)?
+USE_ZOT  = True   # Upload to Zotero?
+
 ########
 # Load libraries
+from box import Box
+
 import importlib
 import pyserpCite
 import yaml
-
-from box import Box
 
 importlib.reload(pyserpCite)
 
@@ -18,17 +23,19 @@ with open("config.yaml", "r") as ymlfile:
     cfg = Box(yaml.safe_load(ymlfile), default_box=True, default_box_attr=None)
 
 API_KEY = cfg.API_KEY
-ZOT_ID = cfg.ZOT_ID
+ZOT_ID  = cfg.ZOT_ID
 ZOT_KEY = cfg.ZOT_KEY
 
 # Instantiate a serpZot object for API management
-citeObj = pyserpCite.serpZot(API_KEY = API_KEY, 
-                           ZOT_ID = ZOT_ID, 
-                           ZOT_KEY = ZOT_KEY)
+citeObj = pyserpCite.serpZot(API_KEY  = API_KEY, 
+                             ZOT_ID   = ZOT_ID, 
+                             ZOT_KEY  = ZOT_KEY)
 
 # Call the search method
 for i in range(len(TERMS)):
-    print(citeObj.searchScholar(TERM = TERMS[i], MIN_YEAR = MIN_YEAR))
+    print(citeObj.searchScholar(TERM     = TERMS[i], 
+                                MIN_YEAR = MIN_YEAR,
+                                SAVE_BIB = SAVE_BIB))
     print("This should've returned 0 (sucess)")
     # Upload the parsed results
     print(citeObj.search2Zotero())
