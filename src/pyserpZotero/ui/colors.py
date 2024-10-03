412cd27d72b896381f1ffe59a162ef2175610b03
Jason Miller <your-correct-email@example.com>
2024-10-03 18:55:35 -0400

    added color scheme

---
+++ src/pyserpZotero/ui/colors.py
@@ -0,0 +1,18 @@
+# ui/colors.py
+
+# Background colors
+bg_main = '#8B4513'  # Warm earth tone
+bg_light = '#CD853F'
+bg_dark = '#5F3813'
+
+# Text colors
+text_main = 'white'
+text_secondary = '#FFD700'  # Golden text color
+
+# Button colors
+btn_primary_bg = '#D2691E'
+btn_secondary_bg = '#A0522D'
+
+# Log text colors
+log_bg = '#FFE4C4'
+log_fg = 'black'
