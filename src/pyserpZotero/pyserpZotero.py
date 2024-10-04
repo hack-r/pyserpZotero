c21c38825a2c1349b87440a4302d55b304d7cd0b
Jason Miller <hello@libreagora.com>
2024-10-04 01:44:20 -0400

    updated version number in terminal art

--- src/pyserpZotero/pyserpZotero.py
+++ src/pyserpZotero/pyserpZotero.py
@@ -105,7 +105,7 @@ def main():
     print(f"{Colors.GREEN}*  Your go-to solution for managing Zotero  *{Colors.ENDC}")
     print(f"{Colors.GREEN}*     entries directly from your terminal.  {Colors.GREEN}*")
     print(f"{Colors.GREEN}*                                           *{Colors.ENDC}")
-    print(f"{Colors.GREEN}*{Colors.BLUE}      Version: {Colors.RED}1.1.2{Colors.ENDC}                       {Colors.GREEN}*{Colors.ENDC}")
+    print(f"{Colors.GREEN}*{Colors.BLUE}      Version: {Colors.RED}1.2.0{Colors.ENDC}                       {Colors.GREEN}*{Colors.ENDC}")
     print(f"{Colors.GREEN}*********************************************{Colors.ENDC}")
 
     script_dir_config_path   = Path(__file__).resolve().parent / 'config.yaml'
